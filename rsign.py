251e1e0b5bd1e40f23292630595d3ea6aeaa22a8
Michael Elsdörfer <michael@elsdoerfer.com>
2014-01-19 07:03:54 +0100

    Add rsign.py script, equivalent to rsign.js.

---
+++ rsign.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import json
+
+import sys
+from ripple import sign_transaction
+from ripple.sign import create_signing_hash
+from ripple.serialize import serialize_object
+
+
+def main(argv):
+    if len(argv) <= 1:
+        print "Usage: rsign.py <secret> <json>"
+        print
+        print 'Example: rsign.py ssq55ueDob4yV3kPVnNQLHB6icwpC \'{"TransactionType":"Payment","Account":"r3P9vH81KBayazSTrQj6S25jW6kDb779Gi","Destination":"r3kmLJN5D28dHuH8vZNUZpMC43pEHpaocV","Amount":"200000000","Fee":"10","Sequence":1}\''
+        return 1
+
+    secret = argv[1]
+    tx = json.loads(argv[2])
+    signed_tx = sign_transaction(tx.copy(), secret)
+
+    output = {
+        'tx_json': signed_tx,
+        'tx_blob': serialize_object(signed_tx),
+        'tx_signing_hash': create_signing_hash(tx),
+        'tx_unsigned': serialize_object(tx)
+    }
+    print(json.dumps(output, indent=2))
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv) or 0)
