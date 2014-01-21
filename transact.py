ef8a5263fe17071a5c18980e717538aa5f56035a
Michael Elsdörfer <michael@elsdoerfer.com>
2014-01-21 14:10:21 +0100

    transact.py was pretty broken due to debugging, now better.
    Also supports XRP amount formatting now.

--- transact.py
+++ transact.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
+import Queue
 import json
 
 import os
 import sys
 
-from ripple import sign_transaction, Client
-from ripple.client import transaction_hash, Remote
+from ripple import sign_transaction, Client, Amount
+from ripple.client import transaction_hash, Remote, RippleEncoder
 from ripple.sign import get_ripple_from_secret
 
 
@@ -42,7 +43,7 @@ def cmd_pay(remote, secret, destination, amount):
         "TransactionType" : "Payment",
         "Account" : sender,
         "Destination" : destination,
-        "Amount" : "%s" % amount,
+        "Amount" : Amount(amount),
     }
     remote.add_fee(tx)
 
@@ -62,18 +63,22 @@ def cmd_pay(remote, secret, destination, amount):
     else:
         # Signup to the transaction stream so we'll be able to verify
         # the transaction result.
-        result, queue = remote.subscribe(streams=['server'])
+        result, queue = remote.subscribe(streams=['transactions'])
 
         # Submit the transaction.
         remote.submit(tx_blob=tx)
         txhash = transaction_hash(tx)
+        print('Submitted transaction with hash %s, waiting for confirm' % txhash)
 
-        remote.close()
         # Look for the final disposition.
         # TODO: That would actually involve looking at the values though
         # rather than just printing the first status update.
         while True:
-            tx = queue.get(timeout=1)
+            try:
+                tx = queue.get(timeout=1)
+            except Queue.Empty:
+                continue
+
             if tx['transaction']['hash'] == txhash:
                 print('FOUND: %s' % tx)
                 break
